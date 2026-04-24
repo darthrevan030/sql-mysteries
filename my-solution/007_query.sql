1b0a5109f41160da7ea477b0b9933d1288a83c04
darth.revan.030 <smartsamarth03@gmail.com>
2026-04-24 11:52:46 +0800

    add SQL query to retrieve driver and person details related to a specific case

---
+++ my-solution/007_query.sql
@@ -0,0 +1,7 @@
+--007_query.sql
+SELECT drivers_license.id,
+    drivers_license.plate_number,
+    person.name
+FROM drivers_license
+    LEFT JOIN person ON drivers_license.id = person.license_id
+WHERE person.id IN (67318, 28819) -- found the killer
