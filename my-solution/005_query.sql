541809886fb934f74ac2cad9954a89a69ef0f8c4
darth.revan.030 <smartsamarth03@gmail.com>
2026-04-24 10:40:58 +0800

    add SQL query to retrieve member details from get_fit_now_member

---
+++ my-solution/005_query.sql
@@ -0,0 +1,9 @@
+--005_query.sql
+SELECT get_fit_now_member.id,
+    get_fit_now_member.person_id,
+    get_fit_now_member.name
+FROM get_fit_now_member
+    LEFT JOIN person ON get_fit_now_member.person_id = person.id
+    LEFT JOIN get_fit_now_check_in ON get_fit_now_check_in.membership_id = get_fit_now_member.id
+WHERE get_fit_now_check_in.check_in_date LIKE '%0109%'
+    AND get_fit_now_member.membership_start_date <= "20170109"
